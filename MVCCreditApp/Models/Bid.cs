008adf003d52be5b3920076b9ec2564840670dc9
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-10-05 14:00:47 +0300

    MVC Lav 6 complete

--- MVCCreditApp/Models/Bid.cs
+++ MVCCreditApp/Models/Bid.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +10,18 @@ namespace MVCCreditApp.Models
     public class Bid
     {
         public virtual int BidId { get; set; }
+
+        [Required]
+        [DisplayName("Имя заявителя")]
         public virtual string CustomerName { get; set; }
+
+        [Required]
+        [DisplayName("Тип кредита")]
         public virtual string CreditHead { get; set; }
+
+        [DisplayName("Дата подачи заявки")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
         public virtual DateTime BidDate { get; set; }
     }
 }
